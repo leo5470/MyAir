a06252d2d1f6c2c62dd2123da26533b63de00497
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-04-13 01:04:30 +0800

    Initial Commit

---
+++ MyAir/MyAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAirApp.swift
+//  MyAir
+//
+//  Created by Leo Cheng on 2024/4/13.
+//
+
+import SwiftUI
+
+@main
+struct MyAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
