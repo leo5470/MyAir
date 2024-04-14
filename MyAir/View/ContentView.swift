01c6394fa37e40f2910544eebd5257e92a6446eb
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-04-14 11:04:12 +0800

    Load in LocalAQIView

--- MyAir/View/ContentView.swift
+++ MyAir/View/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 16) {
+            Text("MyAir")
+                .font(.system(size: 40, weight: .bold))
+                .padding(.leading, 10)
+            
+            LocalAQIView(stationName: "中正站", aqiValue: "58")
         }
-        .padding()
     }
 }
 
