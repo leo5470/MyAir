5992130bb55dd5378a1afc77f69312bf58601153
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-04-15 11:14:11 +0800

    Add SmallComponentView and Map into ContentView

--- MyAir/View/ContentView.swift
+++ MyAir/View/ContentView.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
+        VStack(alignment: .leading, spacing: 15) {
             Text("MyAir")
                 .font(.system(size: 40, weight: .bold))
-                .padding(.leading, 10)
+                .padding(.leading, 15)
+                .padding(.top, 30)
             
-            LocalAQIView(stationName: "中正站", aqiValue: "58")
+            LocalAQIComponentView(stationName: "中正站", aqiValue: "58")
+                .padding(.horizontal, 15)
+            
+            HStack {
+                SmallComponentView(caption: "更多\n本地資料", symbolName: "mappin")
+                    .padding(.leading,  15)
+                
+                SmallComponentView(caption: "其他\n測站資料", symbolName: "map.fill")
+                    .padding(.trailing, 15)
+            }
+            
+            // Map() can directly be called in iOS 17+ only
+            Map()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding(.horizontal, 15)
+                .padding(.bottom, 15)
         }
     }
 }
