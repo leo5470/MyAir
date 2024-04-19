25e6aedda5d855d821227d4e3cc894db60f5445f
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-04-19 18:17:29 +0800

    Add two custom errors

--- MyAir/Errors/APIError.swift
+++ MyAir/Errors/APIError.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 enum APIError: Error {
+    case NILError
     case ConvertError // Handle general failed conversions
     case LocationError // Handle general Location-related errors
+    case CLLocationError // Handle Core Location-related errors
     case NoDataError // Handle general no-data errors
 }
