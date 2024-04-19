0b8f02145d0f9ab1c64ab17db29a7d0604934a49
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-04-19 18:18:16 +0800

    Complete Main View

--- MyAir/View/ContentView.swift
+++ MyAir/View/ContentView.swift
@@ -9,29 +9,97 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var isLocalDataViewActive = false
+    @State private var isOtherStationViewActive = false
+    @State private var showAlert = false
+    
+    @StateObject private var viewModel: ContentViewModel = ContentViewModel()
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 15) {
-            Text("MyAir")
-                .font(.system(size: 40, weight: .bold))
-                .padding(.leading, 15)
-                .padding(.top, 30)
-            
-            LocalAQIComponentView(stationName: "中正站", aqiValue: "58")
-                .padding(.horizontal, 15)
-            
-            HStack {
-                SmallComponentView(caption: "更多\n本地資料", symbolName: "mappin")
-                    .padding(.leading,  15)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 15) {
+                Text("MyAir")
+                    .font(.system(size: 40, weight: .bold))
+                    .padding(.leading, 15)
+                    .padding(.top, 30)
+                    .onAppear {
+                        Task {
+                            do {
+                                try await viewModel.load()
+                            } catch {
+                                showAlert = true
+                            }
+                        }
+                    }
                 
-                SmallComponentView(caption: "其他\n測站資料", symbolName: "map.fill")
-                    .padding(.trailing, 15)
+                
+                if viewModel.closestAQI == nil {
+                    LocalAQIComponentView(stationName: "", aqiValue: "-1")
+                        .padding(.horizontal, 15)
+                } else {
+                    NavigationLink(value: viewModel.closestAQI!) {
+                        LocalAQIComponentView(stationName: viewModel.closestStation!.siteName, aqiValue: viewModel.closestAQI!.aqi)
+                            .shadow(color: .black.opacity(0.4), radius: 2, x: 0, y: 2)
+                            .shadow(color: .black.opacity(0.1), radius: 1.5, x: 0, y: 0)
+                        .padding(.horizontal, 15)
+                    }
+                }
+                    
+                if viewModel.stations == nil {
+                    SmallComponentView(caption: "其他\n測站資料", symbolName: "map.fill", isNil: true)
+                        .padding(.horizontal, 15)
+                } else {
+                    NavigationLink(value: viewModel.stations!) {
+                        SmallComponentView(caption: "其他\n測站資料", symbolName: "map.fill", isNil: false)
+                            .padding(.horizontal, 15)
+                            .shadow(color: .black.opacity(0.4), radius: 2, x: 0, y: 2)
+                        .shadow(color: .black.opacity(0.1), radius: 1.5, x: 0, y: 0)
+                    }
+                }
+                
+                // Map() can directly be called in iOS 17+ only
+                if viewModel.closestAQI == nil {
+                    Map()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 15)
+                } else {
+                    Map{
+                        UserAnnotation()
+                        
+                        Marker(viewModel.closestStation!.siteName, coordinate: viewModel.closestStationCoordinate!)
+                    }
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .padding(.horizontal, 15)
+                        .padding(.bottom, 15)
+                }
+            }
+            .alert("無法獲取資料，請稍後再試", isPresented: $showAlert)
+            {
+                Button("重新整理") {
+                    showAlert = false
+                    Task {
+                        do {
+                            try await viewModel.load()
+                        } catch {
+                            showAlert = true
+                        }
+                    }
+                }
+            } message: {
+                Text("若網路正常，您仍可查看其他測站資料")
             }
             
-            // Map() can directly be called in iOS 17+ only
-            Map()
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .padding(.horizontal, 15)
-                .padding(.bottom, 15)
+            Spacer()
+                .navigationDestination(for: AQIForSingleLocation.self) { 
+                    aqi in LocalDataView(aqi: aqi)
+                }
+                .navigationDestination(for: [Station].self) {
+                    stations in OtherStationView(stations: stations)
+                }
+                .navigationDestination(for: Station.self) {
+                    station in LocalDataView(id: station.siteID)
+                }
         }
     }
 }
--- MyAir/ViewModel/ContentViewModel.swift
+++ MyAir/ViewModel/ContentViewModel.swift
@@ -8,59 +8,57 @@
 import Foundation
 import CoreLocation
 
-import SwiftLocation
 
 class ContentViewModel: ObservableObject {
-    let MOENVApiKey = ProcessInfo.processInfo.environment["MOENV_API_KEY"] ?? ""
+    // Consider performing secret management here.
+    let MOENVApiKey = "7d1cba85-cbee-42b1-903a-4ec9624ee571"
+    let location = Location()
     
     @Published var userLocation: CLLocation? = nil
     @Published var stations: [Station]? = nil
     @Published var closestStation: Station? = nil
+    @Published var closestStationCoordinate: CLLocationCoordinate2D? = nil
     @Published var closestAQI: AQIForSingleLocation? = nil
     
     func load() async throws{
+        // Publishing changes from background threads is not allowed;
+        // make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
         do {
-            userLocation = try await getUserLocation()
-            closestStation = try await findClosestStation()
+            
+            let fetchedStations = try await fetchStations()
+            
+            await MainActor.run {
+                stations = fetchedStations
+            }
+            
+            let fetchedUserLocation = try location.requestCurrentLocation()
+            await MainActor.run {
+                userLocation = fetchedUserLocation
+            }
+            
+            let fetchedClosestStation = try await findClosestStation()
+            await MainActor.run {
+                closestStation = fetchedClosestStation
+            }
+            
+            let fetchedCoordinate = try getClosestStationCoordinate()
+            
             // Force unwrap since if station cannot be found, the error would be caught first
-            closestAQI = try await fetchAQI(siteID: closestStation!.siteID)
+            let fetchedClosestAQI = try await fetchAQI(siteID: closestStation!.siteID)
+            await MainActor.run {
+                closestStationCoordinate = fetchedCoordinate
+                closestAQI = fetchedClosestAQI
+            }
+            
+            // Debug
+            print("Userdata loaded")
+            print("Lat: \(userLocation!.coordinate.latitude), Lon: \(userLocation!.coordinate.longitude)")
+            print("Station: \(closestStation!.siteName)")
+            print("AQI: \(closestAQI!.aqi)")
         } catch {
-            print("Error occurred while trying to initialize ContentViewModel: \(error)")
+            print("Error occurred while trying to initialize ContentViewModel: \(String(describing: error))")
+            throw error
         }
-    }
-    
-    func getUserLocation() async throws -> CLLocation? {
-        let location = Location()
-        var currentLocation: CLLocation?
-        do {
-            // SwiftLocation
-            try await location.requestPermission(.whenInUse) // obtain the permissions
-            currentLocation = try await location.requestLocation().location // get the location
-        } catch {
-            print("Error occurred while trying to obtain location: \(error)")
-            throw APIError.LocationError
-        }
-        return currentLocation
-    }
-    
-    
-    func fetchAQI(siteID: String) async throws -> AQIForSingleLocation {
-        guard let url = URL(string: "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=\(MOENVApiKey)") else {
-            print("Invalid URL")
-            throw URLError(.badURL)
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let responseData = try JSONDecoder().decode(AQIForMultipleLocations.self, from: data)
-        guard let target = responseData.records.first(where: {$0.siteID == siteID}) else {
-            throw APIError.NoDataError
-        }
-        return target
     }
     
     func fetchStations() async throws -> [Station] {
@@ -84,12 +82,14 @@ class ContentViewModel: ObservableObject {
         if userLocation == nil {
             throw APIError.LocationError
         }
+        if stations == nil {
+            throw APIError.NoDataError
+        }
         
-        let stations: [Station] = try await fetchStations()
-        var closestStation: Station = stations[0]
+        var closestStation: Station = stations![0]
         var closestDistance: Double = Double.greatestFiniteMagnitude
-        for station in stations {
-            if station == stations[0] {
+        for station in stations! {
+            if station == stations![0] {
                 continue
             }
             let longitude = station.longitude
@@ -114,5 +114,27 @@ class ContentViewModel: ObservableObject {
             }
         }
         return closestStation
+    }
+    
+    func getClosestStationCoordinate() throws -> CLLocationCoordinate2D {
+        if closestStation == nil {
+            throw APIError.NILError
+        }
+        
+        let longitude = closestStation!.longitude
+        let latitude = closestStation!.latitude
+        
+        guard let longitudeDouble: Double = Double(longitude) else {
+            print("Unable to convert longtitude in Station to double.")
+            throw APIError.ConvertError
+        }
+        
+        guard let latitudeDouble = Double(latitude) else {
+            print("Unable to convert latitude in Station to double.")
+            throw APIError.ConvertError
+        }
+        
+        let coord = CLLocationCoordinate2D(latitude: latitudeDouble, longitude: longitudeDouble)
+        return coord
     }
 }
